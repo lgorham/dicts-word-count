f31e6c5174d8a3bf3842182e6092474921faba3f
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 12:36:56 -0700

    Implemented word_count function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,17 @@
-# put your code here.
+def word_count(filename):
+    """"
+    Takes a file and parses each words and prints out how many times the word appears
+    """
+
+    file_data = open(filename)
+    occurance = {}
+    for line in file_data:
+        line = line.rstrip().replace("--", " ").split(" ")
+        for word in line:
+            word = word.strip("?,!.;-\":_").lower()
+            occurance[word] = occurance.get(word, 0) + 1
+    for word, count in occurance.items():
+        print word, count
+        
+
+word_count("twain.txt")
